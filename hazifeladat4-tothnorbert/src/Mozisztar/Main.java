4fc9b02d7e1bdae48f046ff6833557def727b860
tothnorbert7 <43538265+tothnorbert7@users.noreply.github.com>
2018-11-19 12:17:54 +0100

    Update Main.java

--- hazifeladat4-tothnorbert/src/Mozisztar/Main.java
+++ hazifeladat4-tothnorbert/src/Mozisztar/Main.java
@@ -5,10 +5,45 @@
  */
 package Mozisztar;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
 /**
  *
  * @author Norbert
  */
 public class Main {
+   
+    public static void main(String[] args) {
+        List<Színész> szl = new ArrayList();
+
+        Scanner sc = new Scanner(System.in);
+        String sor = sc.nextLine();
+        while (sor.length() > 0) {
+            String[] darabolt = sor.split("[():,]");
+            for (int i = 3; i < darabolt.length; i++) {
+                Színész sz = new Színész(darabolt[i]);
+
+                if (szl.contains(sz)) {
+                    szl.get(szl.indexOf(sz)).getFilmek().add(new Film(darabolt[0], Integer.parseInt(darabolt[1])));
+                } else {
+                    sz.getFilmek().add(new Film(darabolt[0], Integer.parseInt(darabolt[1])));
+                    szl.add(sz);
+                }
+
+            }
+
+            sor = sc.nextLine();
+
+        }
+
+        
+        Collections.sort(szl);
+        for(Színész i:szl)
+            System.out.println(i);
+
+    }
     
 }
